408e7212e5909ab4b5739a5f0a91b9c9493b2a8e
fresanov <filip.resanovic@yahoo.com>
2023-04-09 21:21:14 +0200

    Add tests for error handling

--- order/internal/application/core/api/api_test.go
+++ order/internal/application/core/api/api_test.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/fresanov/microservices/order/internal/application/core/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type mockedPayment struct {
@@ -50,4 +54,61 @@ func Test_Should_Place_Order(t *testing.T) {
 		CreatedAt: 0,
 	})
 	assert.Nil(t, err)
+}
+
+func Test_Should_Return_Error_When_Db_Persistence_Fail(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+	payment.On("Charge", mock.Anything).Return(nil)
+	db.On("Save", mock.Anything).Return(errors.New("connection error"))
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "phone",
+				UnitPrice:   14.7,
+				Quantity:    1,
+			},
+		},
+		CreatedAt: 0,
+	})
+	assert.EqualError(t, err, "connection error")
+}
+
+func Test_Should_Return_Error_When_Payment_Fail(t *testing.T) {
+	payment := new(mockedPayment)
+	db := new(mockedDb)
+
+	// construct grpc error
+	st := status.New(codes.InvalidArgument, "insufficient balance")
+	desc := "insufficient balance"
+	v := &errdetails.BadRequest_FieldViolation{
+		Description: desc,
+	}
+	br := &errdetails.BadRequest{}
+	br.FieldViolations = append(br.FieldViolations, v)
+	st, _ = st.WithDetails(br)
+
+	payment.On("Charge", mock.Anything).Return(st.Err())
+	db.On("Save", mock.Anything).Return(nil)
+
+	application := NewApplication(db, payment)
+	_, err := application.PlaceOrder(domain.Order{
+		CustomerID: 123,
+		OrderItems: []domain.OrderItem{
+			{
+				ProductCode: "bag",
+				UnitPrice:   2.5,
+				Quantity:    6,
+			},
+		},
+		CreatedAt: 0,
+	})
+	st, _ = status.FromError(err)
+	t.Logf("error: %v\n", err)
+	assert.Equal(t, "order creation failed", st.Message())
+	assert.Equal(t, "insufficient balance", st.Details()[0].(*errdetails.BadRequest).FieldViolations[0].Description)
+	assert.Equal(t, codes.InvalidArgument, st.Code())
 }
